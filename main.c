d4ff97acdc8ed6e0e668683ec786ea4912750ed8
Ali Alwan <ali.alwan0@gmail.com>
2025-09-10 19:02:32 +0200

    added game struct and some boilerplate?

--- main.c
+++ main.c
@@ -4,14 +4,29 @@
 int main()
 {
     bool game_is_running;
+    SDL_Event event;
 
     initialize_SDL();
 
 
 
     while (game_is_running)
-    {
+    {        
+        // Events
+
+        while (SDL_PollEvent(&event))
+        {
+            if (&event.type == SDL_EVENT_QUIT)
+            {
+                game_is_running = false;
+            }
+            
+        }
         
+        // Logic
+        // Render
+        SDL_RenderClear(g_game.renderer);
+        SDL_RenderPresent(g_game.renderer);
     }
     
 
--- main.h
+++ main.h
@@ -5,5 +5,22 @@
 #include <SDL3/SDL.h>
 
 
+typedef struct game
+{
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+
+    SDL_Texture* Background;
+}game;
+game g_game;
+
+typedef struct enemy
+{
+
+}enemy;
+
+
+
+
 
 void initialize_SDL();
